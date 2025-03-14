5333f3cc064871d97fd1258c3649316690a48157
Tanmoy Debnath <154241417+TanmoyFRu@users.noreply.github.com>
2025-03-14 13:18:48 +0530

    Added Set-01 exercises

---
+++ Task-1/Set-1/10.IndexingString.py
@@ -0,0 +1,11 @@
+#In Python, indexing and slicing are techniques used to access specific characters or parts of a string. Indexing means referring to an element of an iterable by its position whereas slicing is a feature that enables accessing parts of the sequence.
+
+# Indexing in Python starts from 0. The first element of a string has an index of 0, the second element has an index of 1, and so on. You can also use negative indexing, where the last element has an index of -1, the second-to-last element has an index of -2, and so on.
+
+string = "Hello, World!"
+print(string[4])  # Output: o
+print(string[-1])  # Output: !
+
+# In this code snippet, the string "Hello, World!" is defined. The character at index 4 is accessed using string[4], which is 'o'. The character at index -1 is accessed using string[-1], which is '!'. The output of the code snippet is 'o' and '!'.
+
+# The code snippet demonstrates how to access specific characters in a string using indexing in Python.
--- Task-1/Set-1/9.Mutable&Imutable.py
+++ Task-1/Set-1/9.Mutable&Imutable.py
@@ -1,13 +1,32 @@
 # For String
 
 string = "Hello World"
-string[4] = "s"
+# ==> string[4] = "s"
 print(string)
 # In the code snippet, the string is defined as "Hello World". The value at the index 4 is changed to "s". However, strings are immutable, and you cannot change the value of an element in a string. Therefore, the code will raise an error. 
 
 # For Tuple
 
 tuple= (1, 2, 3, 4, 5)
-tuple[4] = 6
+# ==> tuple[4] = 6
 print(tuple)
 # In the code snippet, the tuple is defined with the values (1, 2, 3, 4, 5). The value at the index 4 is changed to 6. However, tuples are immutable, and you cannot change the value of an element in a tuple. Therefore, the code will raise an error.
+
+# For List
+
+list = [1, 2, 3, 4, 5]
+list.append(6)
+print(list)
+# In the code snippet, the list is defined with the values [1, 2, 3, 4, 5]. The append() method is used to add the value 6 to the end of the list. The output will be [1, 2, 3, 4, 5, 6].
+
+list = [1, 2, 3, 4, 5]
+list.insert(2, 6)
+print(list)
+# In the code snippet, the list is defined with the values [1, 2, 3, 4, 5]. The insert() method is used to insert the value 6 at index 2. The output will be [1, 2, 6, 3, 4, 5].
+
+# For Dictionary
+
+dir = {"Name": "tanmoy", "Age": 22}
+dir["Age"] = 23
+print(dir)
+# In the code snippet, the list is defined with the values [1, 2, 3, 4, 5]. The append() method is used to add the value 6 to the end of the list. The output will be [1, 2, 3, 4, 5, 6].
